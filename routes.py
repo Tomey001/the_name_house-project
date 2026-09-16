03a13ce7e163b87c6cd2d05941f610713602f571
Bless Daniel Tomey <bdtomey@st.ug.edu.gh>
2026-09-16 09:44:17 +0000

    Fix typo logout_url to logout_user

--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 # routes.py
 from flask import render_template, redirect, url_for, request, flash
 from flask_login import (
-    login_user, logout_url, logout_user,
+    login_user, logout_user,
     login_required, current_user
 )
 from app import app, db
